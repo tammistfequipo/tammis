c0242f423adcec1a37fe323f603b5dab4360ca8a
tammistfequipo <juliolavtel@gmail.com>
2025-07-17 16:15:32 -0300

    Update verificar-codigo.js

--- api/verificar-codigo.js
+++ api/verificar-codigo.js
@@ -2,20 +2,16 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ mensaje: 'Método no permitido' });
-  }
-
-  const { codigo } = req.body;
+  const { codigo } = req.query;
 
   const filePath = path.join(process.cwd(), 'api', 'codigos.json');
-  const rawData = fs.readFileSync(filePath, 'utf-8');
-  const codigos = JSON.parse(rawData);
+  const codigos = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
-  if (codigos[codigo]) {
-    const mensaje = codigos[codigo];
-    return res.status(200).json({ valido: true, mensaje: "✅ " + mensaje });
+  const mensaje = codigos[codigo];
+
+  if (mensaje) {
+    res.status(200).json({ valido: true, mensaje });
   } else {
-    return res.status(200).json({ valido: false, mensaje: "❌ Código inválido. Probá con otro." });
+    res.status(200).json({ valido: false });
   }
 }
